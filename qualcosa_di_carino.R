537100e783096a20ea06530dccd9c85d1b9c8be6
VitoPalm <84906343+VitoPalm@users.noreply.github.com>
2026-05-31 16:35:35 +0200

    Scatterrrrrr and Corrrrrelation

--- qualcosa_di_carino.R
+++ qualcosa_di_carino.R
@@ -1,20 +1,23 @@
 # Weighs 0 Bytes (or, does it? *moon men starts playing*)
+library(corrplot)       # guess
+library(psych)          # corPlot
+
 
 # 0.1.
-ds = read.csv("Dataset_N7.csv", header=T)
-n <- nrow(ds)
-attach(ds)
+twist = read.csv("Dataset_N7.csv", header=T)
+n = nrow(twist)
+attach(twist)
 
 
 # 1.1.
-ds_sorted = ds[order(y_IQ), ]
+twist_sorted = twist[order(y_IQ), ]
 
-head(ds_sorted)
-tail(ds_sorted)
+head(twist_sorted)
+tail(twist_sorted)
 
 
 # 1.2.
-summary(ds)
+summary(twist)
 
 
 # 1.3.
@@ -39,13 +42,34 @@ boxplot(x1_ISO, x2_T, x3_MP, x4_CF, x5_F, x6_GSI, x7_UA,
 )
 
 
-#
-plot(ds)
-cc = cor(ds)
-corrplot(cc, method = 'ellipse')
-corrplot.mixed(cc, number.cex=0.8, tl.cex=0.8)
+# 1.5.
+plot(x1_ISO, y_IQ)
+plot(x2_T, y_IQ)
+plot(x3_MP, y_IQ)
+plot(x4_CF, y_IQ)
+plot(x5_F, y_IQ)
+plot(x6_GSI, y_IQ)
+plot(x7_UA, y_IQ)
 
-corPlot(cc, cex = 1.1, show.legend=TRUE, main="Correlation plot")
+independent_twist = twist
+independent_twist$y_IQ = NULL
+plot(independent_twist)
+
+plot(x7_UA, x5_F)
+plot(x4_CF, x5_F)
+plot(x5_F, x3_MP)
+plot(x1_ISO, x2_T)
+
+# plot(twist)
+
+
+# 1.6.
+cv = cov(twist)
+cr = cor(twist)
+corrplot(cr, method = 'ellipse')
+corrplot.mixed(cr, number.cex=0.8, tl.cex=0.8)
+
+corPlot(cr, cex = 1.1, show.legend=TRUE, main="Correlation plot")
 
 
 
@@ -56,5 +80,5 @@ plot(x5_F, x7_UA)
 
 
 # 99.
-reg = lm(y_IQ ~ x1_ISO + x2_T + x3_MP + x4_CF + x5_F + x6_GSI + x7_UA, data = ds); summary(reg)
-fff = step(y_I, lm(y_IQ ~ x1_ISO + x2_T + x3_MP + x4_CF + x5_F + x6_GSI + x7_UA + I(x1_ISO^2) + I(x2_T^2) + I(x3_MP^2) + I(x4_CF^2) + I(x5_F^2) + I(x6_GSI^2) + I(x7_UA^2) + (x1_ISO + x2_T + x3_MP + x4_CF + x5_F + x6_GSI + x7_UA)^2, data = ds), direction = "both", trace = 1, k = log(n)); summary(fff)
+reg = lm(y_IQ ~ x1_ISO + x2_T + x3_MP + x4_CF + x5_F + x6_GSI + x7_UA, data = twist); summary(reg)
+fff = step(y_I, lm(y_IQ ~ x1_ISO + x2_T + x3_MP + x4_CF + x5_F + x6_GSI + x7_UA + I(x1_ISO^2) + I(x2_T^2) + I(x3_MP^2) + I(x4_CF^2) + I(x5_F^2) + I(x6_GSI^2) + I(x7_UA^2) + (x1_ISO + x2_T + x3_MP + x4_CF + x5_F + x6_GSI + x7_UA)^2, data = twist), direction = "both", trace = 1, k = log(n)); summary(fff)
--- rrrrrreporrrrrrt.md
+++ rrrrrreporrrrrrt.md
@@ -4,20 +4,71 @@
 - Yapping
 
 ## 0.1. Import del Dataset
-- You know the drill
+<!--- You know the drill ---> 
+Per prima cosa è stato importato il dataset in esame (`read.csv()`).
 
 # 1. Analisi Statistica Descrittiva
 - Introductory thingy to the theoretical concept
+La popolazione in analisi è costituita da 100 (`n = nrow(ds)`) campioni
+di cui sono definite varie caratteristiche
+
+- popolazione completamente nota
+- dati di tipo quantitativo continuo
+- obiettivi svolgimento analisi descrittiva
 
 ## 1.1. Visualizzazione dei Dati agli Estremi della Variabile Dipendente
 - Useless
 
 ## 1.2. Summary
 - Comment the data
+- presentazione dei dati 
+- indici di tendenza centrale: media, mediana (boh mostra i dati)
+- indici di dispersione: varianza (e il cugino), escursione campionaria (range)
 
 ## 1.3. Istogrammi
 - Explain how you choose number of thinghies 🪣
+- Denote clear noticeable thinghies
+- Shits and giggles
 
 ## 1.4. Boxplot (Whiskers Diagram)
 - Consider doing fancy shit for labels
 - Notiamo che non ci sono outliers?
+- Other noticeable thinghies (hanno i baffi)
+
+## 1.5. Scatter Plot
+- Grandi quelli con Y
+- independent twist
+- honorable mentions
+- ipotesi su why forme all throughout
+
+
+## 1.6. (covariances too) CORRELATION = CAUSATION SHIT NEEDS BETTER NAME (CONTROVERSIAL)
+- cov just cause we know it by name
+- correlation (con tabella di interpretazioni)
+- comment on correlation (may integrate next entry)
+- comment on scatter plot hypotheses though comments on correlation
+- may need to have to take a look at second order (gl)
+
+## 1.A. Conclusioni
+- prediction on next steps
+- I look forward to see the evolution of the honorable mentions from the scatter plots
+
+
+# 2. Regression
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+7. Conclusioni Finali
+the journey matters more than the destination
